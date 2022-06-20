978c28aa37de6fe4137538b4d379fcbe3d0be840
lich070192 <andrea.stragiot@gmail.com>
2022-06-20 16:54:33 +0100

    added PPO

--- reinforcement/lib_for_dqn/lifecycle_discrete.py
+++ reinforcement/lib_for_dqn/lifecycle_discrete.py
@@ -1,23 +1,23 @@
 import gym
 from gym.spaces import Discrete, Dict, Box
-import numpy as np
-
 
 class LifecycleEnv(gym.Env):
     def __init__(self):
+        # Here we define the choices on consumption and equity allocation
+        self.action_space = Dict({"equity_allocation": Discrete(6),
+                                  "consumption": Discrete(190)})
         # Set starting variables
-        self.wealth = np.zeros(1)
+        self.wealth = 100
         self.starting_income = 100
         self.income = self.starting_income
         self.starting_age = 19
         self.age = self.starting_age
         self.retirement_age = 65
         self.terminal_age = 115
+        self.max_wealth = 10000
+        self.wealth_buckets = 500
         # Here we create our observation space
-        self.observation_space = Box(np.array([self.starting_age, 0]), np.array(
-            [self.terminal_age, np.inf]), shape=(2,), dtype=np.float32)
-        # Here we define the choices on consumption and equity allocation
-        self.action_space = Box(np.array([0, 0]), np.array([1, np.inf]))
+        self.observation_space = Dict({"age": Discrete(self.terminal_age - self.starting_age + 1), "wealth_bucket": Discrete(self.wealth_buckets)})
 
     def step(self, action):
         reward = 0
@@ -29,13 +29,13 @@ class LifecycleEnv(gym.Env):
         risk_free_return = 0.01
         # transformation
         # print(action)
-        action_equity_allocation = action[0]
-        action_consumption = action[1]
+        action_equity_allocation = action["equity_allocation"] / 5
+        action_consumption = action["consumption"] + 10
         # Apply action
         # print(action_equity_allocation)
-        portfolio_return = (risk_return *
-                            action_equity_allocation) + (risk_free_return *
-                                                         (1 - action_equity_allocation))
+        portfolio_return = (risk_return * \
+            action_equity_allocation) + (risk_free_return * \
+            (1 - action_equity_allocation))
         start_wealth = self.wealth
         # Here income is added only after the portfolio return
         self.wealth = (1 + portfolio_return) * (self.wealth -
@@ -54,10 +54,9 @@ class LifecycleEnv(gym.Env):
         # Time passes (philosophical consideration here)
         self.age += 1
         # Placeholder for info
-        info = {"age": self.age, "start_wealth": start_wealth, "wealth": self.wealth,
-                "consumption": action_consumption, "equity_allocation": action_equity_allocation,
-                "port_return": portfolio_return}
-        state = np.array([self.age, self.wealth], dtype=np.float32)
+        info = {"age": self.age, "start_wealth": start_wealth, "wealth": self.wealth, 
+                "consumption": action_consumption, "equity_allocation": action_equity_allocation, "port_return": portfolio_return}
+        state = {"age": self.age, "wealth_bucket": min(int(self.wealth/(self.max_wealth + 1) * self.wealth_buckets), self.wealth_buckets - 1)}
         return state, reward, done, info
 
     def render(self, mode='human'):
@@ -66,8 +65,8 @@ class LifecycleEnv(gym.Env):
 
     def reset(self):
         # Resetting age and wealth
-        self.wealth = 0
+        self.wealth = 100
         self.age = self.starting_age
         self.income = self.starting_income
-        state = np.array([self.age, self.wealth], dtype=np.float32)
+        state = {"age": self.age, "wealth_bucket": int(self.wealth/self.max_wealth)}
         return state
