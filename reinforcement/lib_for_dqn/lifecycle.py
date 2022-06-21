d8e4eb08771e869a168b47b64ba2a9c1cdfe73bd
Andrea <andrea.stragiotti@blackrock.com>
2022-06-21 18:45:59 +0100

    additional changes to make PPO more stable

--- reinforcement/lib_for_dqn/lifecycle.py
+++ reinforcement/lib_for_dqn/lifecycle.py
@@ -13,11 +13,11 @@ class LifecycleEnv(gym.Env):
         self.age = self.starting_age
         self.retirement_age = 65
         self.terminal_age = 115
-        # Here we create our observation space
+        # Observatioon space: age, from starting age to terminal age and wealth, from 0 to infinite wealth
         self.observation_space = Box(np.array([self.starting_age, 0]), np.array(
-            [self.terminal_age, np.inf]), shape=(2,), dtype=np.float32)
+            [self.terminal_age, np.inf]), shape=(2,))
         # Here we define the choices on consumption and equity allocation
-        self.action_space = Box(np.array([0, 0]), np.array([1, np.inf]))
+        self.action_space = Box(np.array([0, 0]), np.array([1, 1]))
 
     def step(self, action):
         reward = 0
@@ -28,21 +28,18 @@ class LifecycleEnv(gym.Env):
         risk_return = 0.05
         risk_free_return = 0.01
         # transformation
-        # print(action)
         action_equity_allocation = action[0]
-        action_consumption = action[1]
+        action_consumption = action[1] * self.wealth
         # Apply action
-        # print(action_equity_allocation)
         portfolio_return = (risk_return *
                             action_equity_allocation) + (risk_free_return *
                                                          (1 - action_equity_allocation))
-        start_wealth = self.wealth
         # Here income is added only after the portfolio return
         self.wealth = (1 + portfolio_return) * (self.wealth -
                                                 action_consumption) + self.income
         if self.wealth < 0:
             self.wealth = 0
-            reward += -10000
+            reward += -1000
             done = True
         reward += action_consumption
         # Complete step if agent is older than terminal age
@@ -54,8 +51,9 @@ class LifecycleEnv(gym.Env):
         # Time passes (philosophical consideration here)
         self.age += 1
         # Placeholder for info
-        info = {"age": self.age, "start_wealth": start_wealth, "wealth": self.wealth,
-                "consumption": action_consumption, "equity_allocation": action_equity_allocation,
+        info = {"age": self.age, "wealth": self.wealth,
+                "consumption": action_consumption, 
+                "equity_allocation": action_equity_allocation,
                 "port_return": portfolio_return}
         state = np.array([self.age, self.wealth], dtype=np.float32)
         return state, reward, done, info
