3a98d23722fd0468b5f01ae275038d043f84a7ae
Andrea <andrea.stragiotti@blackrock.com>
2022-06-27 16:30:06 +0100

    expanding optimal

--- reinforcement/lib_for_dqn/lifecycle.py
+++ reinforcement/lib_for_dqn/lifecycle.py
@@ -6,15 +6,16 @@ import numpy as np
 class LifecycleEnv(gym.Env):
     def __init__(self, const_consumption_level=0.8):
         # Set starting variables
-        self.wealth = 0
+        self.wealth = 1
         self.starting_income = 1
         self.income = self.starting_income
-        self.starting_age = 19
+        self.starting_age = 20
         self.age = self.starting_age
         self.retirement_age = 65
-        self.terminal_age = 115
+        self.terminal_age = 100
         self.last_consumption = 0
         self.const_consumption_level = const_consumption_level
+        self.minimum_consumption = 0.1
         # define market returns
         self.risk_return = 0.05
         self.risk_free_return = 0.01
@@ -23,7 +24,7 @@ class LifecycleEnv(gym.Env):
                                      np.array([self.terminal_age, np.inf]),
                                      shape=(2,))
         # Here we define the choices on consumption and equity allocation
-        self.action_space = Box(np.array([0, 0]), np.array([1, 1]))
+        self.action_space = Box(np.array([-1, -1]), np.array([1, 1]))
 
     def step(self, action):
         done = False
@@ -31,8 +32,8 @@ class LifecycleEnv(gym.Env):
         if self.age >= self.retirement_age:
             self.income = 0
         # transformation
-        action_equity_allocation = action[0]
-        action_consumption = action[1] * self.wealth
+        action_equity_allocation = (action[0] + 1) / 2
+        action_consumption = ((action[1] + 1) / 2) * self.wealth
         # Apply action
         portfolio_return = ((self.risk_return * action_equity_allocation) +
                             (self.risk_free_return * (1 - action_equity_allocation)))
@@ -41,24 +42,30 @@ class LifecycleEnv(gym.Env):
             (self.wealth - action_consumption) + self.income
         reward = action_consumption
         # penalize reward if consumption falls over a certain threshold
-        if action_consumption < (self.last_consumption * self.const_consumption_level):
-            reward += (action_consumption - self.last_consumption) * 100
+        #if action_consumption < (self.last_consumption * self.const_consumption_level):
+        #    reward += -100
+        if action_consumption < (self.starting_income * 0.1):
+            reward += -10
+        if action_consumption > (self.starting_income * 0.9):
+            reward += -10
         # terminal conditions: check if agent is broke or dead
         if self.wealth < 0:
             self.wealth = 0
+            reward += -10
             done = True
         if self.age == self.terminal_age:
             done = True
             reward = 0
-        # Time passes (philosophical consideration here)
         self.last_consumption = action_consumption
-        self.age += 1
         info = {"age": self.age,
                 "wealth": self.wealth,
                 "consumption": action_consumption,
                 "equity_allocation": action_equity_allocation,
-                "port_return": portfolio_return}
+                "port_return": portfolio_return,
+                "reward": reward}
         state = np.array([self.age, self.wealth], dtype=np.float32)
+        # Time passes (philosophical consideration here)
+        self.age += 1
         return state, reward, done, info
 
     def render(self, mode='human'):
@@ -67,7 +74,7 @@ class LifecycleEnv(gym.Env):
 
     def reset(self):
         # Resetting age and wealth
-        self.wealth = 0
+        self.wealth = 1
         self.age = self.starting_age
         self.income = self.starting_income
         self.last_consumption = 0
